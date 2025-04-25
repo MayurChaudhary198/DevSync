0bbac07e4ef5551688a6d71247895dae961b08e9
MayurChaudhary198 <chaudharymayur198@gmail.com>
2025-04-25 17:25:06 +0530

    Initial DevSync project setup

---
+++ server/models/Project.js
@@ -0,0 +1,31 @@
+const mongoose = require("mongoose");
+
+const projectSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        required: true
+    },
+    description: {
+        type: String
+    },
+    createdBy: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref : "User",
+        required: true
+    },
+    teamMembers: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User"
+    }],
+    tasks: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Task"
+    }],
+    createdAt: {
+        type: Date,
+        default: Date.now
+    }
+})
+
+const Project = mongoose.model("Project", projectSchema);
+module.exports = Project;
